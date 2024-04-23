5ea73606d269e168fc0e3de778849f26969bd676
PolloLopez <pollolopeza@gmail.com>
2024-04-23 13:57:57 -0300

    css mejorado

--- js/main.js
+++ js/main.js
@@ -4,53 +4,6 @@ function toggleTexto() {
     textoAdicional.classList.toggle("oculto");
 }
 
-/* BOTON DARK MODE */
-const botonColorMode = document.querySelector("#color-mode");
-const body = document.body;
-
-let darkMode = localStorage.getItem("dark-mode");
-
-// Función del botón para activar el modo oscuro
-function activarDarkMode() {
-    body.classList.add("dark-mode");
-    localStorage.setItem("dark-mode", "activado");
-    actualizarTextoModo();
-}
-
-// Función del botón para desactivar el modo oscuro
-function desactivarDarkMode() {
-    body.classList.remove("dark-mode");
-    localStorage.setItem("dark-mode", "desactivado");
-    actualizarTextoModo();
-}
-
-// Iniciar con el modo oscuro si está activado
-if (darkMode === "activado") {
-    activarDarkMode();
-} else {
-    desactivarDarkMode();
-}
-
-// Función para cambiar el texto del botón según el modo
-function actualizarTextoModo() {
-    const botonColorMode = document.getElementById("color-mode");
-    if (body.classList.contains("dark-mode")) {
-        botonColorMode.textContent = "Claro";
-    } else {
-        botonColorMode.textContent = "Oscuro";
-    }
-}
-
-// Lógica para activar/desactivar el modo oscuro al hacer clic en el botón
-botonColorMode.addEventListener("click", () => {
-    if (body.classList.contains("dark-mode")) {
-        desactivarDarkMode();
-    } else {
-        activarDarkMode();
-    }
-});
-
-
 /*
 // Agregar un evento de escucha para el botón "Finalizar Compra"
 document.getElementById('btn-finalizar-compra').addEventListener('click', function () {
